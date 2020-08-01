17ed78c76723c8c0f1a9b389b9f3131e04893a24
Shailesh <ShaileshTech@outlook.com>
2020-07-31 19:50:53 -0500

    First time checking, Singleton Pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            if(s1 == s2)
+            {
+                Console.WriteLine("Both objects are same");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        //constructor should alwasy be private
+        private Singleton()
+        { 
+        }
+
+        public static Singleton Instance()
+        {
+            //use lazy initialization
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
